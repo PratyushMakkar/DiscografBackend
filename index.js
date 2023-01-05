3d4b42e097ceff1678b6144ab67481a06cf5161e
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-01-05 17:38:52 -0400

    Created start script

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 const {sequalize} = require('./sequelize/SequelConfig')
 const cors = require('cors');
 
+const PORT = process.env.PORT
+
 var id = require('./routes/id')
 var wordcloud = require('./routes/wordcloud')
 
@@ -10,10 +12,10 @@ var app = express();
 app.use('/id', id)
 app.use('/wordcloud', wordcloud)
 app.use(cors({
-  origin: 'https://www.section.io'
+  origin: '0.0.0.0'
 }));
 
-app.listen(2000, function () {
-  console.log('UWEngKid backend listening on port 3000!');
+app.listen(PORT, function () {
+  console.log(`UWEngKid backend listening on port ${PORT}`);
 });
 
